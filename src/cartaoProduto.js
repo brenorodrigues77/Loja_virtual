36fd6fe44407f058a806cc355f6c99c63fe14c5e
brenorodrigues77 <breno7rodrigues@gmail.com>
2024-08-24 22:48:54 -0300

    Loja_virtual

---
+++ src/cartaoProduto.js
@@ -0,0 +1,28 @@
+import { catalogo } from "./utilidades";
+import { adicionarAoCarrinho } from "./MenuCarrinho";
+
+export function renderizarCatalogo() {
+    for (const produtocatalogo of catalogo) {
+        const cartaoProduto = `<div class='border-solid w-48 m-2 flex flex-col p-2 justify-between shadow-xl shadow-slate-500 rounded-lg group' 
+        id='card-produto-${produtocatalogo.id}'>
+    <img 
+    src="./produtos eecommerce/camisetas/${produtocatalogo.nomeArquivoImagem}" 
+      alt="produto 1 do brenoshop."
+      class="group-hover:scale-110 duration-300 my-3 rounded-lg"
+    />
+    <p class='text-sm'>${produtocatalogo.marca}</p>
+    <p class='text-sm'>${produtocatalogo.nome}</p> 
+    <p class='text-sm'>$${produtocatalogo.preco}</p>
+    <button id='adicionar-${produtocatalogo.id}'class='bg-slate-950 hover:bg-slate-700 text-slate-200'
+    ><i class="fa-solid fa-cart-plus"></i></button>
+    </div>`;
+
+        document.getElementById("container-produto").innerHTML += cartaoProduto;
+    }
+
+    for (const produtocatalogo of catalogo) {
+        document
+            .getElementById(`adicionar-${produtocatalogo.id}`)
+            .addEventListener("click", () => adicionarAoCarrinho(produtocatalogo.id));
+    }
+}
---
+++ src/menuCarrinho.js
@@ -0,0 +1,45 @@
+import { catalogo } from "./utilidades";
+
+function abrirCarrinho() {
+  document.getElementById("carrinho").classList.add("right-[0px]");
+  document.getElementById("carrinho").classList.remove("right-[-360px]");
+
+}
+
+function fecharcarrinho() {
+  document.getElementById("carrinho").classList.remove("right-[0px]");
+  document.getElementById("carrinho").classList.add("right-[-360px]");
+}
+
+export function inicializarCarrinho() {
+  const botaoFecharCarrinho = document.getElementById("Fechar-Carrinho")
+  const botaoAbrirCarrinho = document.getElementById("abrir-Carrinho")
+
+  botaoFecharCarrinho.addEventListener("click", fecharcarrinho);
+  botaoAbrirCarrinho.addEventListener("click", abrirCarrinho);
+}
+
+export function adicionarAoCarrinho(idProduto) {
+  const produto = catalogo.find((p) => p.id === idProduto);
+  const containerProdutorCarrinho =
+    document.getElementById("produtos-carrinho");
+  const CartaoProdutoCarrinho = `<article class="flex bg-slate-200 rounded-lg p-1 relative">
+    <button id="Fechar-Carrinho" class="absolute top-0 right-2">
+      <i class="fa-solid fa-circle-xmark text-slate-500 hover:text-slate-900"
+      ></i>
+    </button>
+    <img 
+      src="./produtos eecommerce/camisetas/${produto.nomeArquivoImagem}"
+      alt="Carrinho: ${produto.nome}"
+      class="h-24 rounded-lg">
+    <div class="p-2 flex flex-col justify-between">
+      <p class="text-slate-900 text-sm"> ${produto.nome}</p>
+      <p class="text-slate-400 text-xs"> Tamanho M </p>
+      <p class="text-green-700 text-lg"> $${produto.preco}</p>
+    </div>
+    <div>
+       <button>
+    </div>
+  </article>`;
+  containerProdutorCarrinho.innerHTML += CartaoProdutoCarrinho;
+}
